8f4c44f6336f93d871daf22d038d952309c25eb0
Dmk1999s <108387240+Dmk1999s@users.noreply.github.com>
2024-04-19 17:02:33 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,19 @@
+j = 2
+
+for i in range(1,10):
+    while(j <= 5):
+        print(f'{j} x {i} = {j*i}',end = '\t')
+        j += 1
+    print()
+    j = 2
+
+print()
+print('-----------------------------------------')
+print()
+j = 6
+for i in range(1,10):
+    while(j <= 9):
+        print(f'{j} x {i} = {j*i}',end = '\t')
+        j += 1
+    print()
+    j = 6
